9f078a4f9bdadfbd4ce988f3f9054a2afddc5b96
Sergio <201801656@est.umss.edu>
2025-05-07 15:18:00 -0400

    modificacion variable entorno

--- server.js
+++ server.js
@@ -12,6 +12,4 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
