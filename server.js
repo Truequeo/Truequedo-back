21d05fd45c16917cbb1bd8189b7e2df0000e2b85
Sergio <201801656@est.umss.edu>
2025-05-07 15:01:56 -0400

    HacerDeploy

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
