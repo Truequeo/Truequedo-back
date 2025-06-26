e3dab9af4153796f4d588d13f6e0811611f32eac
Sergio <201801656@est.umss.edu>
2025-06-26 12:43:04 -0400

    configuracionBD

--- connections/database.js
+++ connections/database.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+/*const pool = new Pool({
   host: 'localhost',
   port: 5432,
   database: 'Truequeo',
   user: 'postgres',
   password: 'truequeo',
-});
+});*/
 
+const pool = new Pool({
+  host: 'db.jjynhasmnfkmhtvumcpi.supabase.co',
+  port: 5432,
+  database: 'Truequeo',
+  user: 'postgres',
+  password: 'truequeo123',
+});
 module.exports = { pool };
  
